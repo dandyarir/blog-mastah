712c1d3f99eb22f7170882f72611782b24216323
Faldy Ikhwan Fadila <faldy.if@gmail.com>
2018-10-13 00:24:12 +0700

    Add all

---
+++ app/User.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App;
+
+use App\Models\Blog;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    protected $with = ['blogs'];
+
+    public function blogs()
+    {
+        return $this->hasMany(Blog::class);
+    }
+}
